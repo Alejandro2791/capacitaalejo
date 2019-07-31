9a1945c2d3bafa6acbfeab9bbd489e9d28086db6
Alejandro Osorio <0alessx0@gmail.com>
2019-07-30 20:43:13 -0500

    Formato de Tabla 1

--- index.php
+++ index.php
@@ -21,9 +21,9 @@
     </header>
     <div id="Contenido">
     <section>
-        <table>
-        <tr><td>
-        <p>Lorem ipsum dolor sit amet, sapien etiam, nunc amet dolor ac odio mauris justo. Luctus arcu, urna praesent at id quisque ac. Arcu es massa vestibulum malesuada, integer vivamus elit eu mauris eus, cum eros quis aliquam wisi. Nulla wisi laoreet suspendisse integer vivamus elit eu mauris hendrerit facilisi, mi mattis pariatur aliquam pharetra eget.</p>
+        <table id=Tabla1>
+        <tr><td id=Titulo_tabla1>
+        ¿Quienes somos?
         </td></tr>
         </table>   
     </section>
