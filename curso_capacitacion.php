4d6089eaa690490b5e23af8deeb2a785a726d31b
Alejandro Osorio <0alessx0@gmail.com>
2019-07-31 06:43:56 -0500

    Creacion de Paginas

---
+++ curso_capacitacion.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Capacita Alejo</title>
+     <!-- Temas-->
+    <link rel="stylesheet" type="text/css" href="estilo.css">
+</head>
+    <body>
+        <nav><table>
+            <tr>
+            <td>Nosotros</td>
+            <td>Curso de Capacitación</td>
+            <td>Talleres</td>
+            <td>Diplomados</td>
+            <td>Conferencias</td>
+            <td>Contacto</td>
+            </tr>
+    </table></nav>
+    <header>
+        <table><tr><td><h1>Capacitación Alejos<h1></td></tr></table>
+    </header>
+    <div id="Contenido">
+    <section>
+        <table id="Tabla1" align="center">
+        <tr><td id="Titulo_tabla1">
+        ¿Quienes somos?
+        </td></tr>
+        <tr><td id="Parrafo_tabla1">
+        Lorem ipsum dolor sit amet, sapien etiam, nunc amet dolor ac 
+        odio mauris justo. Luctus arcu, urna praesent at id quisque ac.
+         Arcu es massa vestibulum malesuada, integer vivamus elit eu mauris 
+         eus, cum eros quis aliquam wisi. Nulla wisi laoreet suspendisse integer 
+         vivamus elit eu mauris hendrerit facilisi, mi mattis pariatur aliquam 
+         pharetra eget.
+        </td></tr>
+        </table>
+        <br><br>
+        <table id="Tabla2"><tr><td id="Titulo_tabla2">Nuestra Filosofia</td></tr></table>
+        <hr size="2" color=#000000 width="850"/>
+        <table id="Tabla3" align="center">
+            <tr><th>Misión</th>
+            <th>Visión</th></tr>
+            <tr><td>Lorem ipsum dolor sit amet, sapien etiam, nunc amet 
+                     dolor ac odio mauris justo. Luctus arcu, urna praesent 
+                     at id quisque ac. Arcu es massa vestibulum malesuada, 
+                     integer vivamus elit eu mauris eus, cum eros quis aliquam 
+                     wisi. Nulla wisi laoreet suspendisse integer vivamus elit
+                      eu mauris hendrerit facilisi, mi mattis pariatur aliquam 
+                      pharetra eget.</td>
+                 <td>Lorem ipsum dolor sit amet, sapien etiam, nunc amet 
+                     dolor ac odio mauris justo. Luctus arcu, urna praesent 
+                     at id quisque ac. Arcu es massa vestibulum malesuada, 
+                     integer vivamus elit eu mauris eus, cum eros quis aliquam 
+                     wisi. Nulla wisi laoreet suspendisse integer vivamus elit
+                      eu mauris hendrerit facilisi, mi mattis pariatur aliquam 
+                      pharetra eget.</td></tr>
+        <table>
+        <hr size="2" color=#000000 width="850"/>      
+    </section>
+    </div>
+    <br><br><br><br><br><br><br><br>
+    <footer>
+    <table id="Tabla4">
+        <tr><td id="Parrafo_tabla4">Lorem ipsum dolor sit amet, sapien etiam, nunc amet dolor 
+            ac odio mauris justo. Luctus arcu, urna praesent at id quisque <br>
+            ac. Arcu es massa vestibulum malesuada, integer vivamus elit eu
+             mauris eus, cum eros quis a</td></tr>
+</table>
+    </footer>
+    </body>
+</html>
