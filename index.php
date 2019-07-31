c5438e3d8a5b64e4c8919e56c9fe226baa5912e8
Alejandro Osorio <0alessx0@gmail.com>
2019-07-30 22:08:22 -0500

    Texto

--- index.php
+++ index.php
@@ -25,6 +25,14 @@
         <tr><td id=Titulo_tabla1>
         ¿Quienes somos?
         </td></tr>
+        <tr><td>
+        Lorem ipsum dolor sit amet, sapien etiam, nunc amet dolor ac 
+        odio mauris justo. Luctus arcu, urna praesent at id quisque ac.
+         Arcu es massa vestibulum malesuada, integer vivamus elit eu mauris 
+         eus, cum eros quis aliquam wisi. Nulla wisi laoreet suspendisse integer 
+         vivamus elit eu mauris hendrerit facilisi, mi mattis pariatur aliquam 
+         pharetra eget.
+        </td></tr>
         </table>   
     </section>
     </div>
