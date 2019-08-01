aa41102b55e62554cdb2e8214dfa38ff6a657aa9
Alejandro Osorio <0alessx0@gmail.com>
2019-07-31 20:55:27 -0500

    Contacto

---
+++ contacto.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Capacita Alejo</title>
+     <!-- Temas-->
+    <link rel="stylesheet" type="text/css" href="estilo.css">
+</head>
+    <body>
+        <nav><table>
+        <tr>
+            <td><a href="index.php">Nosotros</a></td>
+            <td><a href="curso_capacitacion.php">Curso de Capacitación</a></td>
+            <td><a href="talleres.php">Talleres</a></td>
+            <td><a href="diplomados.php">Diplomados</a></td>
+            <td><a href="conferencias.php">Conferencias</a></td>
+            <td>Contacto</td>
+            </tr>
+    </table></nav>
+    <header>
+        <table><tr><td><h1>Conferencias<h1></td></tr></table>
+    </header>
+    <div id="Contenido">
+    <section>
+        <table id="Tabla1" align="center">
+        <br><br>
+        <table id="Tabla2"><tr><td id="Titulo_tabla2">Contactos</td></tr></table>
+        
+        <table id="Tabla3" align="center">
+            <tr><th id="contacto">Contactanos</th>
+            <th>Ubicación</th></tr>
+            <tr><td id="contacto">Lorem ipsum dolor sit amet, sapien etiam, nunc amet 
+                     dolor ac odio mauris justo. Luctus arcu, urna praesent 
+                     at id quisque ac. Arcu es massa vestibulum malesuada, 
+                     integer vivamus elit eu mauris eus, cum eros quis aliquam 
+                     wisi. Nulla wisi laoreet suspendisse integer vivamus elit
+                      eu mauris hendrerit facilisi, mi mattis pariatur aliquam 
+                      pharetra eget.</td>
+                 <td><hr size="2" color=#000000 width="850"/>
+
+                    <hr size="2" color=#000000 width="850"/>  
+                 </td></tr>
+        <table>
+            
+    </section>
+    </div>
+    <br><br><br><br><br><br><br><br>
+    <footer>
+    <table id="Tabla4">
+        <tr><td id="Parrafo_tabla4">Lorem ipsum dolor sit amet, sapien etiam, nunc amet dolor 
+            ac odio mauris justo. Luctus arcu, urna praesent at id quisque <br>
+            ac. Arcu es massa vestibulum malesuada, integer vivamus elit eu
+             mauris eus, cum eros quis a</td></tr>
+</table>
+    </footer>
+    </body>
+</html>
