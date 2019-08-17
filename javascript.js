9a503346b5ce45ce1035640325ae033c0c288d81
ALEJANDRO <0alessx0@gmail.com>
2019-08-17 13:39:58 -0500

    Reiniciar

--- javascript.js
+++ javascript.js
@@ -1,3 +1,3 @@
 function formLogin() { 
-window.showModalDialog("login.php", "Iniciar Sesion", "width=80px", "height=120px");
+window.open("login.php", "Iniciar Sesion", "width=80px", "height=120px", "toolbar=no", "location=no", "resizable=no") 
 }
