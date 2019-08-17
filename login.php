4dc5a8f8d30994072be8e4e41369406f88bf6416
ALEJANDRO <0alessx0@gmail.com>
2019-08-17 13:56:06 -0500

    Login

--- login.php
+++ login.php
@@ -8,13 +8,19 @@
 	<script type="text/javascript" src="javascript.js"></script>
 </head>
     <body>
+    <!-- Titulo -->
+    <header>
+        <table><tr><td><h1>Capacitación Alejos<h1></td></tr></table>
+    </header>
     <!-- Formulario Login -->
     <div class='Formulario_Login'>
     <form action="" method="post" name="FormEntrar">
     <label for='usuario'>Usuario:</label>
     <input type="text" name="user" placeholder="nombre.apellido" required>
+    <br>
     <label for='contrasena'>Contraseña:</label>
     <input type="password" name="contra" placeholder="*****" required>
+    <br>
     <button type="submit" >Entrar</button>
     </form>
     </div>
