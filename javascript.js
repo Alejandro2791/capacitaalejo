561f47602b6815f68430ed47ee04914feec48eb6
ALEJANDRO <0alessx0@gmail.com>
2019-08-17 13:30:39 -0500

    Login

--- javascript.js
+++ javascript.js
@@ -1,3 +1,3 @@
 function formLogin() { 
-window.open("login.php", "Iniciar Sesion", "width=80px", "height=120px", "toolbar=no", "location= no", "resizable=no") 
+window.showModalDialog("login.php", '', 'dialogHeight:450 px;dialogWidth:670px;center:Yes;help:No;resizable: No;status:No;');
 }
