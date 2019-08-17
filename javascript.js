14e46e53492f4bd193a8c00f05783213287ab221
ALEJANDRO <0alessx0@gmail.com>
2019-08-17 13:52:20 -0500

    Tamaño

--- javascript.js
+++ javascript.js
@@ -1,3 +1,3 @@
 function formLogin() { 
-window.open("login.php", "Iniciar Sesion", "menubar=no, width=250, height=200, resizable=no, scrollbars=no, toolbar=no, locationbar=no, tittlebar=no, status=yes") 
+window.open("login.php", "Iniciar Sesion", "width=30px", "height=50px");
 }
