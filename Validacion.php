f3608d65e89259c5295efedabe80e1fc0f7a6920
Alejandro Osorio <0alessx0@gmail.com>
2019-08-18 08:30:20 -0500

    Conexion Servidor

--- Validacion.php
+++ Validacion.php
@@ -43,7 +43,11 @@ class Validacion
     //Funcion de Validacion Usuario
     public function ValidarUsuario(){
         $retorno = false;
-
+        $Database = getenv('database_database');
+        $Host     = getenv('database_host');
+        $Password = getenv('database_password');
+        $Port     = getenv('database_port');
+        $User     = getenv('database_user');
         $db = pg_connect("host=$Host dbname=$Database port=$Port user=$User password=$Password");
 
         //Validar Conexion Base de Datos
