33e524a9328e482b00d22068c8d92125d12bba7d
Alejandro Osorio <0alessx0@gmail.com>
2019-08-18 11:06:03 -0500

    Borrar

--- config.php
+++
@@ -1,8 +0,0 @@
-<?php
-$Database = getenv('database_database');
-$Host     = getenv('database_host');
-$Password = getenv('database_password');
-$Port     = getenv('database_port');
-$User     = getenv('database_user');
-
-?>
