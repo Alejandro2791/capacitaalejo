cc8ea0cbc9f96aafca3153350f6700d7ebe595f9
ALEJANDRO <0alessx0@gmail.com>
2019-08-18 14:01:26 -0500

    Preueba

--- index.php
+++ index.php
@@ -12,6 +12,22 @@ require_once 'Validacion.php';
 </head>
     <body>
         <nav>
+            <!-- Formulario Login -->
+    <form action="" method="post" name="FormLogin">
+    <input type="text" name="user" placeholder="Usuario" required>
+    <br><br>
+    <input type="password" name="contra" placeholder="*****" required>
+    <br><br>
+    <button type="submit" >Entrar</button>
+    <!-- Mensaje -->
+    <?php
+    if(!empty($_POST['user']) && !empty($_POST['contra'])){
+    $iniciar=new Validacion($_POST['user'],$_POST['contra']);
+    $iniciar->Ingresar();
+    echo $iniciar->MostrarMsg();
+    }
+    ?>
+    </form>
 
     <!-- Menu Principal -->
     <table>
@@ -25,25 +41,6 @@ require_once 'Validacion.php';
             <td><a href="login.php">Iniciar Sesión</a></td>
             </tr>
     </table>
-        <!-- Formulario Login -->
-        <div class="Formulario_Login">
-    <form action="" method="post" name="FormLogin">
-        <!-- Mensaje -->
-        <?php
-        if(!empty($_POST['user']) && !empty($_POST['contra'])){
-        $iniciar=new Validacion($_POST['user'],$_POST['contra']);
-        $iniciar->Ingresar();
-        echo $iniciar->MostrarMsg();
-        }
-        ?>
-    <input type="text" name='user' placeholder="Usuario" required>
-    <br><br>
-    <input type="password" name='contra' placeholder="*****" required>
-    <br><br>
-    <button type="submit" >Entrar</button>
-
-    </form>
-    </div>
     </nav>
     <header>
         <table><tr><td><h1>Capacitación Alejos<h1></td></tr></table>
