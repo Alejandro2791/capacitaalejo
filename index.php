d6bae1db18826375331f11a4ea480e7bd8ed37cc
ALEJANDRO <0alessx0@gmail.com>
2019-08-18 14:04:34 -0500

    Prueba

--- index.php
+++ index.php
@@ -12,21 +12,21 @@ require_once 'Validacion.php';
 </head>
     <body>
         <nav>
+        <!-- Mensaje -->
+        <?php
+        if(!empty($_POST['user']) && !empty($_POST['contra'])){
+        $iniciar=new Validacion($_POST['user'],$_POST['contra']);
+        $iniciar->Ingresar();
+        echo $iniciar->MostrarMsg();
+        }
+        ?>
             <!-- Formulario Login -->
     <form action="" method="post" name="FormLogin">
-    <input type="text" name="user" placeholder="Usuario" required>
+    <input type="text" name='user' placeholder="Usuario" required>
     <br><br>
-    <input type="password" name="contra" placeholder="*****" required>
+    <input type="password" name='contra' placeholder="*****" required>
     <br><br>
     <button type="submit" >Entrar</button>
-    <!-- Mensaje -->
-    <?php
-    if(!empty($_POST['user']) && !empty($_POST['contra'])){
-    $iniciar=new Validacion($_POST['user'],$_POST['contra']);
-    $iniciar->Ingresar();
-    echo $iniciar->MostrarMsg();
-    }
-    ?>
     </form>
 
     <!-- Menu Principal -->
