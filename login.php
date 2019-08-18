771a1ae5cc91c6931711f876d9b901de7ec3483b
Alejandro Osorio <0alessx0@gmail.com>
2019-08-18 15:58:38 -0500

    Prueba

--- login.php
+++ login.php
@@ -27,9 +27,11 @@ require_once 'Validacion.php';
     <button type="submit" >Entrar</button>
         <!-- Mensaje -->
         <?php
+        if(!empty($_POST['user']) && !empty($_POST['contra'])){
         $iniciar=new Validacion($_POST['user'],$_POST['contra']);
         $iniciar->Ingresar();
         echo $iniciar->MostrarMsg();
+        }
         ?>
     </form>
     </div>
