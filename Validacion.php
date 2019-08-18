5123031c934bf192f6a4b50cfb0ea0db146aee9b
ALEJANDRO <0alessx0@gmail.com>
2019-08-18 11:14:09 -0500

    Prueba

--- Validacion.php
+++ Validacion.php
@@ -56,7 +56,7 @@ class Validacion
             $this->Mensaje_='<div><strong>Error!!!</strong>Conexion erronea al Servidor</div>';
         }else{
             //Consulta SQL
-            $consulta ="SELECT idusuario, usuario, activo FROM usuarios WHERE usuario='".$this->Usuario_."';";
+            $consulta ="SELECT idusuario, usuario, activo FROM usuarios WHERE usuario='alejandro.osorio';";
             $respuesta = pg_query($db,$consulta);
 
             while($datos = pg_fetch_row($respuesta)){
