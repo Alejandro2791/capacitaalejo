ad2c05ae84c4e214883ec0e153e45d3846469bea
Alejandro Osorio <0alessx0@gmail.com>
2019-08-19 21:52:33 -0500

    Validacion de Formulario

--- alta_usuario.php
+++ alta_usuario.php
@@ -16,7 +16,7 @@ if(preg_match("/[A-Zaz]/", $Nombre)==true){
         if(preg_match("/[A-Zaz]/", $Apellido1)==true){
     
               //Validacion de Correo
-          if(preg_match("/[a-zAZ0-9]+\.[a-zA-Z0-9]/", $Usuario )==true){
+          if(preg_match("/[a-zAZ0-9\_\-]+\@[a-zA-Z0-9]+\.[a-zA-Z0-9]/", $Correo )==true){
     
                 $Database = getenv('database_database');
                 $Host     = getenv('database_host');
@@ -27,41 +27,39 @@ if(preg_match("/[A-Zaz]/", $Nombre)==true){
       
             $sql = "INSERT INTO usuarios (nombre, apellido1, apellido2, usuario, contra, activo) 
                     VALUES ('$Nombre', '$Apellido', '$Apellido1', '$Usuario', '$Contrasena', '1')";
-      
-              if(pg_query($db, $sql)){
-              echo "<script>
-              alert('Alta exitosa');
-              window.location= '/usuarios.php';
-              </script>";
-              }else{
-                echo "<script>
-                alert('Alta erronea');
-                window.location= '/usuarios.php';
-                </script>";
-              }
-              }else{
-                echo "<script>
-                alert('Error - El correo que ingresaste no tiene formato correcto');
-                window.location= '/usuarios.php';
-                </script>";
-              }
+          if(pg_query($db, $sql)){
+            echo "<script>
+            alert('Alta exitosa');
+            window.location= '/usuario.php';
+            </script>";
             }else{
-                echo "<script>
-                alert('Error - El apellido que ingresaste no tiene formato correcto');
-                window.location= '/usuarios.php';
-                </script>";
-              }
-        }else{
-          echo "<script>
-          alert('Error - El apellido que ingresaste no tiene formato correcto');
-          window.location= '/usuarios.php';
-          </script>";
-          }
+              echo "<script>
+              alert('Alta erronea');
+              window.location= '/usuario.php';
+              </script>";
+            }
+            }else{
+              echo "<script>
+              alert('Error - El correo que ingresaste no tiene formato correcto');
+              window.location= '/usuario.php';
+              </script>";
+            }
+          }else{
+              echo "<script>
+              alert('Error - El apellido que ingresaste no tiene formato correcto');
+              window.location= '/usuario.php';
+              </script>";
+            }
       }else{
-          echo "<script>
-          alert('Error - El nombre que ingresaste no tiene formato correcto');
-          window.location= '/usuarios.php';
-          </script>";
+        echo "<script>
+        alert('Error - El apellido que ingresaste no tiene formato correcto');
+        window.location= '/usuario.php';
+        </script>";
         }
-
+    }else{
+        echo "<script>
+        alert('Error - El nombre que ingresaste no tiene formato correcto');
+        window.location= '/usuario.php';
+        </script>";
+      }
 ?>
