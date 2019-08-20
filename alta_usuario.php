6cd9ca804c934d8d8d3a3b0c5b6b3f373890bf33
Alejandro Osorio <0alessx0@gmail.com>
2019-08-19 21:12:31 -0500

    Vald

--- alta_usuario.php
+++ alta_usuario.php
@@ -1,4 +1,62 @@
 <?php
+//Variables de Insertar Datos
+$Nombre =$_POST['nombre'];
+$Apellido =$_POST['apellido1'];
+$Apellido1 =$_POST['apellido2'];
+$Usuario =$_POST['usuario'];
+$Contrasena =$_POST['contra'];
 
+//Validacion de Nombre
+if(preg_match("/[A-Zaz]/", $Nombre)==true){
+
+    //Validacion de Apellido
+      if(preg_match("/[A-Zaz]/", $Apellido)==true){
+    
+        //Validacion de Apellido1
+        if(preg_match("/[A-Zaz]/", $Apellido1)==true){
+    
+              //Validacion de Correo
+          if(preg_match("/[a-zAZ0-9]+\.[a-zA-Z0-9]/", $Usuario )==true){
+    
+            $db = pg_connect("host=$host_ dbname=$Database port=$Port user=$User password=$Password") or die ("Error de Conexion".pg_last_error());
+      
+            $sql = "INSERT INTO usuarios (nombre, apellido1, apellido2, usuario, contra, activo) 
+                    VALUES ('$Nombre', '$Apellido', '$Apellido1', '$Usuario', '$Contrasena', '1')";
+      
+              if(pg_query($db, $sql)){
+              echo "<script>
+              alert('Alta exitosa');
+              window.location= '/usuarios.php';
+              </script>";
+              }else{
+                echo "<script>
+                alert('Alta erronea');
+                window.location= '/usuarios.php';
+                </script>";
+              }
+              }else{
+                echo "<script>
+                alert('Error - El correo que ingresaste no tiene formato correcto');
+                window.location= '/usuarios.php';
+                </script>";
+              }
+            }else{
+                echo "<script>
+                alert('Error - El apellido que ingresaste no tiene formato correcto');
+                window.location= '/usuarios.php';
+                </script>";
+              }
+        }else{
+          echo "<script>
+          alert('Error - El apellido que ingresaste no tiene formato correcto');
+          window.location= '/usuarios.php';
+          </script>";
+          }
+      }else{
+          echo "<script>
+          alert('Error - El nombre que ingresaste no tiene formato correcto');
+          window.location= '/usuarios.php';
+          </script>";
+        }
 
 ?>
