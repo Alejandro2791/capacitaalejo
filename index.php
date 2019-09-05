b7129a7984a09ff682303ff74265abe7c08f8bb5
Alejandro Osorio <0alessx0@gmail.com>
2019-09-05 09:07:03 -0500

    Formato

--- index.php
+++ index.php
@@ -11,21 +11,29 @@
 </head>
     <body>
     <!-- Menu Principal -->
-    <nav class="navbar navbar-inverse">
-        <div class="container-fluid Menu_Principal">
-            <div class="row">
-            <div class="col-sm-2"><a href="index.php">Nosotros</a></div>
-            <div class="col-sm-2"><a href="curso_capacitacion.php">Curso de Capacitación</a></div>
-            <div class="col-sm-2"><a href="talleres.php">Talleres</a></div>
-            <div class="col-sm-2"><a href="diplomados.php">Diplomados</a></div>
-            <div class="col-sm-2"><a href="conferencias.php">Conferencias</a></div>
-            <!-- <div class="col-sm-2"><a href="contacto.php">Contacto</a></div> -->
-            <div class="col-sm-2"><a href="javascript:formLogin();">Iniciar Sesión</a></div>
-        </div>
-
-    </div>
-
-    </nav>
+    Dupdo
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Navbar</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Features</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Pricing</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+      </li>
+    </ul>
+  </div>
+</nav>
     <header>
         <table><tr><td><h1>Capacitación Alejos<h1></td></tr></table>
     </header>
