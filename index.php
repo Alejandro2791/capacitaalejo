0a057af0e8353b766a5a11e221619b92626e6817
Alejandro Osorio <0alessx0@gmail.com>
2019-09-05 05:21:09 -0500

    Formato

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
     <!-- Menu Principal -->
         <nav class="navbar navbar-inverse">
         <div class="container-fluid">
-    <div class="collapse navbar-collapse">
+    <div class="collapse navbar-collapse" id="myNavbar">
         <ul class="nav navbar-nav">
         <li><a href="index.php">Nosotros</a></li>
         <li><a href="curso_capacitacion.php">Curso de Capacitación</a></li>
