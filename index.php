c23b81e1a0756b0157ff4dd5ebd265de0b475f8f
Alejandro Osorio <0alessx0@gmail.com>
2019-09-05 09:00:39 -0500

    Formato

--- index.php
+++ index.php
@@ -10,22 +10,6 @@
     <script type="text/javascript" src="javascript.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
-  <style>
-    /* Remove the navbar's default margin-bottom and rounded borders */ 
-    .navbar {
-      margin-bottom: 0;
-      border-radius: 0;
-    }
-    
-    /* Set height of the grid so .sidenav can be 100% (adjust as needed) */
-    .row.content {height: 450px}
-    
-    /* Set gray background color and 100% height */
-    .sidenav {
-      padding-top: 20px;
-      background-color: #f1f1f1;
-      height: 100%;
-    }
 </head>
     <body>
     <!-- Menu Principal -->
