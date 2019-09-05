5f67b556c2d22eb591938318b6b5e55d42622022
Alejandro Osorio <0alessx0@gmail.com>
2019-09-05 05:03:34 -0500

    Formato

--- index.php
+++ index.php
@@ -6,36 +6,29 @@
     <link rel="stylesheet" type="text/css" href="estilo.css">
     <!-- Bootstrap -->
     <link rel="stylesheet" type="text/css" href="bootstrap-4.3.1-dist/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
     <!--vincular con formulario.js-->
 	<script type="text/javascript" src="javascript.js"></script>
 </head>
     <body>
     <!-- Menu Principal -->
-    <nav class="navbar navbar-inverse">
-  <div class="container-fluid">
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>                        
-      </button>
-      <a class="navbar-brand" href="#">Logo</a>
+        <nav class="navbar navbar-inverse Menu_Principal">
+        <div class="container-fluid">
+    <div class="collapse navbar-collapse">
+        <ul class="nav navbar-nav">
+        <li><a href="index.php">Nosotros</a></li>
+        <li><a href="curso_capacitacion.php">Curso de Capacitación</a></li>
+        <li><a href="talleres.php">Talleres</a></li>
+        <li><a href="diplomados.php">Diplomados</a></li>
+        <li><a href="conferencias.php">Conferencias</a></li>
+        <li><a href="contacto.php">Contacto</a></li>
+        </ul>
+        <ul class="nav navbar-nav navbar-right">
+        <li><a href="javascript:formLogin();">Iniciar Sesión</a></li>
+        </ul>
+        </div>
     </div>
-    <div class="collapse navbar-collapse" id="myNavbar">
-      <ul class="nav navbar-nav">
-        <li class="active"><a href="#">Home</a></li>
-        <li><a href="#">About</a></li>
-        <li><a href="#">Projects</a></li>
-        <li><a href="#">Contact</a></li>
-      </ul>
-      <ul class="nav navbar-nav navbar-right">
-        <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
-      </ul>
-    </div>
-  </div>
-</nav>
+
+    </nav>
     <header>
         <table><tr><td><h1>Capacitación Alejos<h1></td></tr></table>
     </header>
