93459b6954c6270af08be939bd86884d305ceda7
Alejandro Osorio <0alessx0@gmail.com>
2019-09-05 05:16:52 -0500

    Formato

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
 </head>
     <body>
     <!-- Menu Principal -->
-        <nav class="navbar navbar-inverse Menu_Principal">
+        <nav class="navbar navbar-inverse">
         <div class="container-fluid">
     <div class="collapse navbar-collapse">
         <ul class="nav navbar-nav">
