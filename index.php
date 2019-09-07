08ef04b222f7e083bd808cec45513d25a5db2a64
Alejandro Osorio <0alessx0@gmail.com>
2019-09-06 21:57:32 -0500

    Prueba

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <!-- Bootstrap -->
     <link rel="stylesheet" type="text/css" href="bootstrap-4.3.1-dist/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-    <script src="bootstrap-4.3.1-dist/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="bootstrap-4.3.1-dist/js/bootstrap.min.js"></script>
 
     <!--vincular con formulario.js-->
 	<script type="text/javascript" src="javascript.js"></script>
