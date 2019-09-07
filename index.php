0e340e68fd606bfeb59db865254d55a058e65749
Alejandro Osorio <0alessx0@gmail.com>
2019-09-06 21:51:32 -0500

    Responsivo

--- index.php
+++ index.php
@@ -6,6 +6,9 @@
     <link rel="stylesheet" type="text/css" href="estilo.css">
     <!-- Bootstrap -->
     <link rel="stylesheet" type="text/css" href="bootstrap-4.3.1-dist/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="bootstrap-4.3.1-dist/js/bootstrap.min.js"></script>
+
     <!--vincular con formulario.js-->
 	<script type="text/javascript" src="javascript.js"></script>
 </head>
